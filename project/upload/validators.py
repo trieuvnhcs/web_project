4c3ee39a0c8472f21bb5e7bf25fa09a064c907cf
hoangtrieu <vonguyenhoangtrieu72@gmail.com>
2017-12-23 05:18:37 +0700

    second ver

--- project/upload/validators.py
+++ project/upload/validators.py
@@ -2,6 +2,6 @@ def validate_file_extension(value):
     import os
     from django.core.exceptions import ValidationError
     ext = os.path.splitext(value.name)[1]  
-    valid_extensions = ['.pdf', '.doc', '.docx','.odt','.rtf', '.xlsx', '.xls','.ppt','.pptx','.ods','.odp','.txt','.jpg','.png']
+    valid_extensions = ['.pdf', '.doc', '.docx', '.xlsx', '.xls','.ppt','.pptx','.txt']
     if not ext.lower() in valid_extensions:
         raise ValidationError(u'Unsupported file extension.')
